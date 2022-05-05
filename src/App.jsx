81b1b6bd79041295ca4b93a382bead5237770c4d
Alejandro Martín de León <alu0101015941@ull.edu.es>
2022-05-05 12:17:43 +0100

    first component todolist

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import { TodoList } from "./components/TodoList";
+
+export function App() {
+
+  const [todos, setTodos] = useState([
+      { id: 1, task: "Tarea 1", completed: false },
+  ]);
+    return(
+      <TodoList todos={todos} />
+    ) 
+
+}
